f84cfd0bc32a8ba4c9764760646f793517bfba5d
Barbara Krzisnik <81755085+barbarakrzisnik@users.noreply.github.com>
2024-10-17 23:05:15 -0300

    Create Page.tsx

---
+++ app/Page.tsx
@@ -0,0 +1,9 @@
+import MealPlanner from './meal-planner'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <MealPlanner />
+    </main>
+  )
+}
